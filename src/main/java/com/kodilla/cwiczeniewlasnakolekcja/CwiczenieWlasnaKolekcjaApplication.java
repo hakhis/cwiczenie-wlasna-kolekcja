643e91ef08eb038b8a87710ae44643abb426ce96
hakhis <b2mieszkowski@gmail.com>
2020-04-01 20:35:19 +0200

    Przerzuciłem maina do głównej klasy aplikacji Dokończyłem klasę StringCollection Chyba koniec?

--- src/main/java/com/kodilla/cwiczeniewlasnakolekcja/CwiczenieWlasnaKolekcjaApplication.java
+++ src/main/java/com/kodilla/cwiczeniewlasnakolekcja/CwiczenieWlasnaKolekcjaApplication.java
@@ -7,7 +7,41 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CwiczenieWlasnaKolekcjaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CwiczenieWlasnaKolekcjaApplication.class, args);
+        int n = 10;
+        System.out.println("Adding " + n + " new elements at the end of queue");
+        StringCollection collection1 = new StringCollection();
+        for (int i = 0; i < n; i++) {
+            collection1.addElementLast("Element " + i);
+        }
+        System.out.println("Queue size is: " + collection1.getSize());
+        collection1.showCollecction();
+        System.out.println("\nTesting method getElement(n) for elements 0, 5 and 12");
+        System.out.println(collection1.getElement(0));
+        System.out.println(collection1.getElement(5));
+        System.out.println(collection1.getElement(12));
+        System.out.println("\nDeleting element \"Element 4\"");
+        System.out.println(collection1.removeElement("Element 4"));
+        System.out.println("Queue size is: " + collection1.getSize());
+        collection1.showCollecction();
+        System.out.println("\nDeleting element \"Element 0\"");
+        System.out.println(collection1.removeElement("Element 0"));
+        System.out.println("Queue size is: " + collection1.getSize());
+        collection1.showCollecction();
+        System.out.println("\nDeleting element \"Element 9\"");
+        System.out.println(collection1.removeElement("Element 9"));
+        System.out.println("Queue size is: " + collection1.getSize());
+        collection1.showCollecction();
+        System.out.println("\nDeleting element \"Element 12\"");
+        System.out.println(collection1.removeElement("Element 12"));
+        System.out.println("Queue size is: " + collection1.getSize());
+        collection1.showCollecction();
+
+        System.out.println("\nAdding " + n + " new elements at the beginning of queue");
+        StringCollection collection2 = new StringCollection();
+        for (int i = 0; i < n; i++) {
+            collection2.addElementFirst("Element " + i);
+        }
+        collection2.showCollecction();
     }
 
 }
--- src/main/java/com/kodilla/cwiczeniewlasnakolekcja/StringCollection.java
+++ src/main/java/com/kodilla/cwiczeniewlasnakolekcja/StringCollection.java
@@ -3,21 +3,46 @@ package com.kodilla.cwiczeniewlasnakolekcja;
 public class StringCollection {
     Element collectionHead;
     Element pointer;
+    int size;
+
     public StringCollection() {
         this.collectionHead = null;
+        this.size = 0;
     }
-    Element getElement(int n) {
-        pointer = collectionHead;
-        for (int i = 0; i < n; i++) {
 
-        }
+    public int getSize() {
+        return this.size;
     }
-    public void goToLast() {
-        while(pointer.getNext() != null) {
-            pointer = pointer.getNext();
-        }
+
+    String getElement(int n) {
+        if(n <= size) {
+            pointer = collectionHead;
+            if (n == 0) {
+                return pointer.getValue();
+            } else {
+                for (int i = 0; i < n; i++) {
+                    pointer = pointer.getNext();
+                }
+                return pointer.getValue();
+            }
+        } else return "The queue is smaller than " + n + " elements";
     }
-    void addElement(String s) {
+    /*Zrobiłem 2 metody dodające nowy element*/
+    //Dodaje nowy element na początku kolejki
+    public void addElementFirst(String s) {
+        if (collectionHead == null) {
+            collectionHead = new Element(s);
+        } else {
+            pointer = collectionHead;
+            collectionHead = new Element(s);
+            pointer.setPrev(collectionHead);
+            collectionHead.setNext(pointer);
+        }
+        size++;
+    }
+
+    //Dodaje nowy element na końcu kolejki
+    public void addElementLast(String s) {
         if(collectionHead == null) {
             collectionHead = new Element(s);
         } else if(collectionHead.getNext() == null) {
@@ -32,7 +57,9 @@ public class StringCollection {
             pointer.setNext(new Element(s));
             pointer.getNext().setPrev(pointer);
         }
+        size++;
     }
+
     public void showCollecction() {
         pointer = collectionHead;
         while (pointer != null) {
@@ -40,11 +67,27 @@ public class StringCollection {
             pointer = pointer.getNext();
         }
     }
-    /*
+
     boolean removeElement(String s) {
-        if(pointer.getValue() == s) {
+        pointer = collectionHead;
+        while (!pointer.getValue().equals(s)) {
+            if (pointer.getNext() != null) {
+                pointer = pointer.getNext();
+            } else {
+                System.out.println("Element not found");
+                return false;
+            }
+        }
+        if(pointer.getNext() == null) {
+            pointer.getPrev().setNext(null);
+        } else if (pointer.getPrev() == null){
+            pointer.getNext().setPrev(null);
+            collectionHead = pointer.getNext();
+        } else {
+            pointer.getNext().setPrev(pointer.getPrev());
             pointer.getPrev().setNext(pointer.getNext());
         }
+        size--;
+        return true;
     }
-     */
 }
